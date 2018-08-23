fa900ac102dc426c13f1836569db3f6bc2810e8b
hamernik <445720@mail.muni.cz>
2018-08-23 13:34:52 +0200

    Improve project structure and optimize dependencies

--- projects/graph-topology/src/public_api.ts
+++ projects/graph-topology/src/public_api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of graph-topology
  */
 
-export * from './lib/graph/graph.module';
-export * from './lib/graph/force-graph.component';
-export * from './lib/others/topology.config';
+export { GraphModule } from './lib/graph/graph.module';
+export { ForceGraphComponent } from './lib/graph/force-graph.component';
+export { TopologyConfig } from './lib/others/topology.config';
