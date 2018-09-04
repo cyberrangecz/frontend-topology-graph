72c8f46fdcb970794a81d5bd7029261e40067e19
hamernik <445720@mail.muni.cz>
2018-09-04 17:27:52 +0200

    Add test file path to config

--- src/app/graph-topology-config.ts
+++ src/app/graph-topology-config.ts
@@ -4,10 +4,9 @@ export class CustomTopologyConfig extends TopologyConfig {
 
   decoratorsRestUrl = '';
   defaultDecoratorRefreshPeriodInSeconds = 3;
-  //topologyRestUrl = '/assets/sample-data/graph-test-data.json';
+  topologyRestUrl = '/assets/sample-data/graph-test-data.json';
   scenarioRestUrl = 'http://kypo2.ics.muni.cz:5000/scenario/';
   sandboxName = 'sandbox1';
-  topologyRestUrl = 'http://localhost:8080/kypo2-rest-sandbox/api/v1/' + this.sandboxName + '/topology';
   useRealTime = false;
   useDecorators = false;
 }
