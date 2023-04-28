b847215c4a2f3854d8425aa1f43fc796c50e4131
Karolína Dočkalová Burská <396296@mail.muni.cz>
2023-04-28 09:58:36 +0200

    Update topology display

--- projects/kypo-topology-graph/src/lib/model/enums/node-context-menu-items-enum.ts
+++ projects/kypo-topology-graph/src/lib/model/enums/node-context-menu-items-enum.ts
@@ -5,5 +5,5 @@ export enum NodeActionEnum {
   Resume = 'Resume',
   Reboot = 'Reboot',
   Suspend = 'Suspend',
-  CopyHostInfo = 'Copy host info to clipboard',
+  CopyHostInfo = 'Copy node info to clipboard',
 }
